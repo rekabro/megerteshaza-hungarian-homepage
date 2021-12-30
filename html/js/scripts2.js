5eaceed48ba085c58cdc5fb322dbba31c68ecb3e
Reka B. Rochlitz <rnegyzet73@gmail.com>
2021-12-30 23:35:06 +0000

    working on css

---
+++ html/js/scripts2.js
@@ -0,0 +1,6 @@
+/* --- CONTENT --- */
+
+$(".icon").mouseenter(function(){
+    $(".arrow").animate({marginLeft: "-1em"}, 500).animate({marginLeft: "0"});
+});
+
---
+++ script.js
@@ -0,0 +1,30 @@
+/* CONTENTS */
+
+
+/* Show icon-related info-spans */
+
+$(".icons1").hover(function(){
+    $(".hidden1").show();
+  },
+  function(){
+    $(".hidden1").hide();
+  });
+  
+
+$(".icons2").hover(function(){
+    $(".hidden2").show();
+  },
+  function(){
+    $(".hidden2").hide();
+});
+  
+  
+$(".icons3").hover(function(){
+    $(".hidden3").show();
+},
+function(){
+    $(".hidden3").hide();
+}
+    );
+
+  
